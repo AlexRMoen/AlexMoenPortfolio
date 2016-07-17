01294ac58a8d5901a967a8415b51fa0f202b87d4
AlexRMoen <alex.r.moen@gmail.com>
2016-07-16 17:19:55 -0700

    Added Javascript switch to modals
    Now have access to full control over how the switch to modal proceeds.
    Tweaking now…

---
+++ js/expandModal.js
@@ -0,0 +1,109 @@
+// JavaScript Document
+
+function openEllaModal() {
+
+var scrollingToGallery = true;
+
+if (scrollingToGallery == true) {
+(function($) {
+    $(document).ready(function() {
+         $('html, body').animate({
+           'scrollTop':   $('#gallery').offset().top
+         }, 2000);
+		 
+		// makes the background fill the screen
+        document.getElementById('gallery').style.height = '100vh';
+		 
+		
+		 
+		     });
+			 
+			 
+			 
+			 setTimeout(function() {
+ 
+         // closes other elements on screen
+        document.getElementById('bgvid').style.display = 'none';
+        document.getElementById('aboveFold').style.display = 'none';
+        document.getElementById('aboutMe').style.display = 'none';
+        document.getElementById('porthole').style.display = 'none';
+ 
+}, 2200);
+
+
+setTimeout(function() {
+			 
+			 
+        document.getElementById('gallery').style.padding = '0';
+    document.getElementById('myWork').style.opacity = '0';
+    document.getElementById('myWork').style.transform = 'scale(0)';
+    document.getElementById('myWork').style.height = '0';
+    document.getElementById('myWork').style.marginBottom = '0';
+        document.getElementById('firstRow').style.paddingTop = '7.5vh';
+        document.getElementById('title').style.display = 'none';
+        document.getElementById('arrowDown').style.display = 'none';
+        
+
+    
+    // rescales and fades other line 1 items
+    document.getElementById('kojiro').style.height = '85vh';
+    document.getElementById('kojiro').style.width = '0';
+    document.getElementById('kojiro').style.opacity = '0';
+    document.getElementById('kojiro').style.marginLeft = '0';
+    document.getElementById('trello').style.height = '85vh';
+    document.getElementById('trello').style.width = '0';
+    document.getElementById('trello').style.opacity = '0';
+    document.getElementById('trello').style.marginLeft = '0';
+    
+    // rescales and fades line 2 items
+    document.getElementById('playboy').style.height = '0';
+    document.getElementById('playboy').style.opacity = '0';
+    document.getElementById('enterprise').style.height = '0';
+    document.getElementById('enterprise').style.opacity = '0';
+    document.getElementById('chirpr').style.height = '0';
+    document.getElementById('chirpr').style.opacity = '0';
+    document.getElementById('newitem').style.height = '0';
+    document.getElementById('newitem').style.opacity = '0';
+    
+    // displays left and right arrows
+    // NONE document.getElementById('ellaLeftArrow').style.left = '2.5em';
+    // NONE document.getElementById('ellaLeftArrow').style.opacity = '1';
+    document.getElementById('ellaRightArrow').style.right = '2.5em';
+    document.getElementById('ellaRightArrow').style.opacity = '1';
+    
+    // scales a gallery item into a modal
+    document.getElementById('ella').style.height = '85vh';
+    document.getElementById('ella').style.width = '80%';
+    document.getElementById('ella').style.marginLeft = '10%';
+    document.getElementById('ella').style.marginRight = '10%';
+    document.getElementById('ella').style.overflowX = 'hidden';
+    document.getElementById('ella').style.overflowY = 'scroll';
+    document.getElementById('ellaHeader').style.height = '55%';
+    document.getElementById('ellaMain').style.height = '45%';
+    document.getElementById('ellaMain').style.opacity = '1';
+    document.getElementById('ellaCloseX').style.transform = 'scale(1)';
+    document.getElementById('ellaCloseX').style.opacity = '1';
+    document.getElementById('ellaCloseX').style.right = '12vw';
+    document.getElementById('ellaCloseX').style.top = '11vh';
+    document.getElementById('ellaLabel').style.transform = 'scale(1.3)';
+    document.getElementById('ellaLabel').style.bottom = '3.75em';
+	
+	}, 2400);
+	
+	
+	
+			 
+			 
+})(jQuery);
+}
+
+	
+	
+	
+
+
+
+
+
+
+}
