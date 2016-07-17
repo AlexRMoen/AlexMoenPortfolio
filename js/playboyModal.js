9f989c4cab81d9bba6b17f759db443a08dbb193a
AlexRMoen <alex.r.moen@gmail.com>
2016-07-16 23:49:54 -0700

    Playboy modal fixed
    It works.

--- js/playboyModal.js
+++ js/playboyModal.js
@@ -40,64 +40,6 @@ function openPlayboyModal() {
 	// adds the padding to accurately position the modal
     document.getElementById('firstRow').style.paddingTop = '7.5vh';
 
-    // rescales and fades other line 1 items
-    document.getElementById('ella').style.height = '85vh';
-    document.getElementById('ella').style.width = '0';
-    document.getElementById('ella').style.opacity = '0';
-    document.getElementById('ella').style.marginLeft = '0';
-    document.getElementById('kojiro').style.height = '85vh';
-    document.getElementById('kojiro').style.width = '0';
-    document.getElementById('kojiro').style.opacity = '0';
-    document.getElementById('kojiro').style.marginLeft = '0';
-    
-    // rescales and fades line 2 items
-    document.getElementById('playboy').style.height = '0';
-    document.getElementById('playboy').style.opacity = '0';
-    document.getElementById('enterprise').style.height = '0';
-    document.getElementById('enterprise').style.opacity = '0';
-    document.getElementById('chirpr').style.height = '0';
-    document.getElementById('chirpr').style.opacity = '0';
-    document.getElementById('newitem').style.height = '0';
-    document.getElementById('newitem').style.opacity = '0';
-    
-    // displays left and right arrows
-    document.getElementById('trelloLeftArrow').style.left = '2.5em';
-    document.getElementById('trelloLeftArrow').style.opacity = '1';
-    document.getElementById('trelloRightArrow').style.right = '2.5em';
-    document.getElementById('trelloRightArrow').style.opacity = '1';
-    
-    // scales a gallery item into a modal
-    document.getElementById('trello').style.height = '85vh';
-    document.getElementById('trello').style.width = '80%';
-    document.getElementById('trello').style.marginLeft = '10%';
-    document.getElementById('trello').style.marginRight = '10%';
-    document.getElementById('trello').style.overflowX = 'hidden';
-    document.getElementById('trello').style.overflowY = 'scroll';
-    document.getElementById('trelloHeader').style.height = '55%';
-    document.getElementById('trelloMain').style.height = '45%';
-    document.getElementById('trelloMain').style.opacity = '1';
-    document.getElementById('trelloCloseX').style.transform = 'scale(1)';
-    document.getElementById('trelloCloseX').style.opacity = '1';
-    document.getElementById('trelloCloseX').style.right = '12vw';
-    document.getElementById('trelloCloseX').style.top = '11vh';
-    document.getElementById('trelloLabel').style.transform = 'scale(1.3)';
-    document.getElementById('trelloLabel').style.bottom = '3.75em';
-	
-	}, 500);
-		 
-})(jQuery);
-
-
-}
-
-
-
-// makes the background fill the screen
-    document.getElementById('gallery').style.height = '110vh';
-    document.getElementById('myWork').style.opacity = '0';
-    document.getElementById('myWork').style.transform = 'scale(0)';
-    document.getElementById('firstRow').style.paddingTop = '7.5vh';
-    
     // rescales and fades line 1 items
     document.getElementById('ella').style.height = '0';
     document.getElementById('ella').style.opacity = '0';
@@ -134,7 +76,21 @@ function openPlayboyModal() {
     document.getElementById('playboy').style.width = '80%';
     document.getElementById('playboy').style.marginLeft = '10%';
     document.getElementById('playboy').style.marginRight = '10%';
-    document.getElementById('playboyHeader').style.height = '30%';
-    document.getElementById('playboyMain').style.height = '70%';
+    document.getElementById('playboy').style.overflowX = 'hidden';
+    document.getElementById('playboy').style.overflowY = 'scroll';
+    document.getElementById('playboyHeader').style.height = '55%';
+    document.getElementById('playboyMain').style.height = '45%';
     document.getElementById('playboyMain').style.opacity = '1';
     document.getElementById('playboyCloseX').style.transform = 'scale(1)';
+    document.getElementById('playboyCloseX').style.opacity = '1';
+    document.getElementById('playboyCloseX').style.right = '12vw';
+    document.getElementById('playboyCloseX').style.top = '11vh';
+    document.getElementById('playboyLabel').style.transform = 'scale(1.3)';
+    document.getElementById('playboyLabel').style.bottom = '3.75em';
+	
+	}, 500);
+		 
+})(jQuery);
+
+
+}
