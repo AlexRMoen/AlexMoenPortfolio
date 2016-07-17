a391cba49b7f8f1f51dcd2e6388c6c8d625127dd
AlexRMoen <alex.r.moen@gmail.com>
2016-07-17 12:15:44 -0700

    Created Close X function
    It works. The only issue is that it places you at the top of the page
    afterward. A solution is forthcoming.

--- js/ellaModal.js
+++ js/ellaModal.js
@@ -91,6 +91,92 @@ function ellaOpenModal() {
 }
 
 
+function ellaCloseX() {
+	
+	
+	
+	
+	
+	// adds the padding to accurately position the modal
+    document.getElementById('firstRow').style.paddingTop = '0';
+	
+	// removes all padding from the gallery
+	// document.getElementById('gallery').style.padding = '0';
+	
+	// rescales and fades-in other line 1 items
+    document.getElementById('kojiro').style.height = '35vh';
+    document.getElementById('kojiro').style.width = '32.2666666667%';
+    document.getElementById('kojiro').style.opacity = '1';
+    document.getElementById('kojiro').style.marginLeft = '1.6%';
+    document.getElementById('trello').style.height = '35vh';
+    document.getElementById('trello').style.width = '32.2666666667%';
+    document.getElementById('trello').style.opacity = '1';
+    document.getElementById('trello').style.marginLeft = '1.6%';
+	
+	// rescales and fadesin line 2 items
+    document.getElementById('playboy').style.height = '35vh';
+    document.getElementById('playboy').style.opacity = '1';
+    document.getElementById('enterprise').style.height = '35vh';
+    document.getElementById('enterprise').style.opacity = '1';
+    document.getElementById('chirpr').style.height = '35vh';
+    document.getElementById('chirpr').style.opacity = '1';
+    document.getElementById('newitem').style.height = '35vh';
+    document.getElementById('newitem').style.opacity = '1';
+	
+	// removes left and right arrows
+    // NONE document.getElementById('ellaLeftArrow').style.left = '-2.5em';
+    // NONE document.getElementById('ellaLeftArrow').style.opacity = '1';
+    document.getElementById('ellaRightArrow').style.right = '-2.5em';
+    document.getElementById('ellaRightArrow').style.opacity = '0';
+	
+	// scales a gallery item into a modal
+    document.getElementById('ella').style.height = '35vh';
+    document.getElementById('ella').style.width = '32.2666666667%';
+    document.getElementById('ella').style.marginLeft = '0';
+    document.getElementById('ella').style.marginRight = '0';
+    document.getElementById('ella').style.overflowX = 'auto';
+    document.getElementById('ella').style.overflowY = 'auto';
+    document.getElementById('ellaHeader').style.height = '100%';
+    document.getElementById('ellaMain').style.height = '0';
+    document.getElementById('ellaMain').style.opacity = '0';
+    document.getElementById('ellaCloseX').style.transform = 'scale(0)';
+    document.getElementById('ellaCloseX').style.opacity = '0';
+    document.getElementById('ellaCloseX').style.right = '91.5333333333%';
+    document.getElementById('ellaCloseX').style.top = '50%';
+    document.getElementById('ellaLabel').style.transform = 'scale(1)';
+    document.getElementById('ellaLabel').style.bottom = '1.75em';
+	
+	// adds the gallery title
+    document.getElementById('myWork').style.height = 'auto';
+    document.getElementById('myWork').style.marginBottom = '1em';
+		 
+	// adds the visual of the gallery title
+	document.getElementById('myWork').style.opacity = '1';
+    document.getElementById('myWork').style.transform = 'scale(1)';
+	
+	// opens other elements on screen
+    document.getElementById('aboveFold').style.display = 'block';
+    document.getElementById('title').style.display = 'block';
+    document.getElementById('arrowDown').style.display = 'block';
+    document.getElementById('aboutMe').style.display = 'block';
+    document.getElementById('porthole').style.display = 'block';
+	
+	// starts the video
+	document.getElementById('bgvid').style.display = 'block';
+	
+	    $(document).ready(function() {
+		 // scrolls to the top of the gallery div
+         $('html, body').animate({
+           'scrollTop':   $('#gallery').offset().top
+         }, 1000); 
+	});
+	
+}
+
+
+	
+	
+
 function ellaRightArrow() {
 	
 // closes itself
